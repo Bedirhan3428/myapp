34beef45f437090d65c1d4f34f720f2b4fdae21e
Bedirhan <abimer2350@gmail.com>
2025-08-20 20:13:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const bakim = true;
  * ve bu sayının kişi listesi uzunluğuna göre modunu alarak geçerli kişi indeksini döndürür.
  */
 function getValidDayIndex() {
-  const startDate = new Date("2025-07-19"); // Mehmet Enes'ten başlayacak şekilde ayarlandı
+  const startDate = new Date("2025-07-15"); // Mehmet Enes'ten başlayacak şekilde ayarlandı
   const today = new Date();
 
   let count = 0;
